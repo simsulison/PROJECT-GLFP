f8d82f057b3ca5d0e32f598764bf88b2530e3e3a
심의석 <simsuli9@naver.com>
2021-02-11 16:47:21 +0900

    feat : #16 Create member CRUD api

--- src/main/java/glfp/glfp/GlfpApplication.java
+++ src/main/java/glfp/glfp/GlfpApplication.java
@@ -2,7 +2,9 @@ package glfp.glfp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -11,5 +13,8 @@ public class GlfpApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GlfpApplication.class, args);
 	}
-
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
 }
--- src/main/java/glfp/glfp/controller/MemberController.java
+++ src/main/java/glfp/glfp/controller/MemberController.java
@@ -5,10 +5,10 @@ import glfp.glfp.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
 
@@ -16,9 +16,29 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @GetMapping("/member/{member_id}") //조회
+    @GetMapping("/{member_id}") //조회
     public ResponseEntity<MemberDto> getMember(@PathVariable("member_id") Long mId){
         MemberDto memberDTO = memberService.getMember(mId);
         return new ResponseEntity<>(memberDTO, HttpStatus.OK);
     }
+
+    @PostMapping("") //등록 ->Post 방식이기 때문에 RequestBody를 통해 HTTP요청 body를 자바 객체로 받을 수 있다.
+    public ResponseEntity<String> register(@RequestBody MemberDto memberDto){
+        memberService.join(memberDto);
+        return new ResponseEntity<>(memberDto.getUserName(), HttpStatus.OK);
+    }
+
+    @PutMapping("") //수정
+    public ResponseEntity<String> revise(@RequestBody MemberDto memberDto){
+        memberService.revise(memberDto);
+        return new ResponseEntity<>(memberDto.getUserName(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{member_id}") //삭제
+    public ResponseEntity<String> delete(@PathVariable("member_id") Long mId){
+        String userName = memberService.getMember(mId).getUserName();
+        memberService.delete(mId);
+        return new ResponseEntity<>(userName, HttpStatus.OK);
+    }
+
 }
